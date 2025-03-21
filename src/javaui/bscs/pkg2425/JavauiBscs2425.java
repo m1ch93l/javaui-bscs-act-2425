fb4f75f145d5d64926d8cd4db40a051c297e64cd
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-03-22 00:37:02 +0800

    Dashboard

--- src/javaui/bscs/pkg2425/JavauiBscs2425.java
+++ src/javaui/bscs/pkg2425/JavauiBscs2425.java
@@ -1,20 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package javaui.bscs.pkg2425;
-
-/**
- *
- * @author m1ch93l
- */
 public class JavauiBscs2425 {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Dashboard().setVisible(true);
     }
     
 }
