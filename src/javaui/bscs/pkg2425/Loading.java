0f98c1c6f2f8c3c75bf2903678e4c8f8bec8ce16
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-04-01 23:08:17 +0800

    Create a second dashboard.

--- src/javaui/bscs/pkg2425/Loading.java
+++ src/javaui/bscs/pkg2425/Loading.java
@@ -1,4 +1,7 @@
 package javaui.bscs.pkg2425;
+
+import java.time.Duration;
+
 public class Loading extends javax.swing.JFrame {
 
     public Loading() {
@@ -10,22 +13,33 @@ public class Loading extends javax.swing.JFrame {
     private void initComponents() {
 
         jProgressBar1 = new javax.swing.JProgressBar();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel1.setText("Please wait for a while");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap(104, Short.MAX_VALUE)
-                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 445, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(84, 84, 84))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 445, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(84, 84, 84))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(224, 224, 224))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(164, Short.MAX_VALUE)
+                .addContainerGap(126, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
                 .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(104, 104, 104))
         );
@@ -37,18 +51,22 @@ public class Loading extends javax.swing.JFrame {
         new Thread(new Runnable(){
             @Override
             public void run() {
-            try {
-                for (int i = 0; i < 101; i++) {
-                jProgressBar1.setValue(i);
-                Thread.sleep(100);
-                }
-            } catch (Exception e) {
-            }
+                try {
+                    for (int i = 0; i < 101; i++) {
+                        jProgressBar1.setValue(i);
+                        Thread.sleep(100);
+                    }
+                        SecondDashboard second = new SecondDashboard();
+                        second.setVisible(true);
+                        dispose();
+                    
+                } catch (Exception e) { }
             }
         }).start();
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JProgressBar jProgressBar1;
     // End of variables declaration//GEN-END:variables
 }
